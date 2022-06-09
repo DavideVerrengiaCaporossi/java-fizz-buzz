7b625f9a3a7aec59c88865a107690b8286824adb
DavideVerrengiaCaporossi <oren93vc@gmail.com>
2022-06-09 16:22:10 +0200

    correzione del precedente push

--- src/Jana60/javafizzbuzz.java
+++ src/Jana60/javafizzbuzz.java
@@ -1,11 +1,19 @@
 package Jana60;
 
 public class javafizzbuzz {
-	public static void main(String[] args) {
-	for (int i = 1; i <= 100; i++) {
-		System.out.println( i );
-		
-		 }
-
-}
+		public static void main(String[] args) {
+			
+				//creazione for che conti e stampi fino a 100
+				for (int i = 1; i <= 100; i++) {
+					System.out.println( i );
+					//creazione if se i e divisibile per 3 stampa fizz
+					if(i % 3 == 0) {
+					System.out.println("Fizz");
+					 }//creazione else if se i e divisibile per 5 stampa fizz
+					else if(i % 5 == 0) {
+						System.out.println("FizzBizz");
+					}
+	
+				}
+		}
 }
